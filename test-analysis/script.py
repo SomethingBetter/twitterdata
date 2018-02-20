f14ba1243bf82e13ce3d4fa6f3a33db4f89c5cda
Matteo Monchiero <matteo@trifacta.com>
2018-02-19 21:21:25 -0800

    python ingestion

--- test-analysis/script.py
+++ test-analysis/script.py
@@ -1,4 +1,4 @@
 import pandas_gbq as bq
 projectid = 'sandbox-161802'
-data_frame = bq.read_gbq('SELECT * FROM twitterdata.example', projectid)
+data_frame = bq.read_gbq('SELECT * FROM twitterdata.example2', projectid)
 print data_frame
